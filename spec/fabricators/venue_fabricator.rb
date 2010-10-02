ef7834008c4437dac0a6bbc4d5d1bda467608ea5
Samuel O. Obukwelu <sam@fcgmedia.com>
2010-10-02 03:44:49 -0400

    added venue fabricator

---
+++ spec/fabricators/venue_fabricator.rb
@@ -0,0 +1,15 @@
+Fabricator :bar, :from => :venue do
+  name      "Aperitivo"
+  address   "279 Fifth Avenue"
+  country   "US"
+  zipcode   "11215"
+  user_id   "4c43475fff808d982a00001a"
+end
+
+Fabricator :lounge, :from => :venue do
+  name      "#{Faker::Company.name} Lounge"
+  address   Faker::Address.street_address
+  country   "US"
+  zipcode   Faker::Address.zip_code
+  user_id   "4c43475fff808d982a00001a"
+end
