43e3a387a70336cc45cefe4cb29a90a982e0ffbf
Samuel O. Obukwelu <sam@fcgmedia.com>
2010-11-04 23:15:46 -0400

    updated model.rb

--- lib/fcg-service-servers/lib/model.rb
+++ lib/fcg-service-servers/lib/model.rb
@@ -1,42 +1,44 @@
-module FCG::Model
-  module ClassMethods
-    def is_paranoid
-      self.send :include, Mongoid::Paranoia
-    end
+module FCG
+  module Model
+    module ClassMethods
+      def is_paranoid
+        self.send :include, Mongoid::Paranoia
+      end
     
-    def is_versioned(versions=5)
-      self.send :include, Mongoid::Versioning
-      max_versions versions
-    end
+      def is_versioned(versions=5)
+        self.send :include, Mongoid::Versioning
+        max_versions versions
+      end
     
-    def has_transitions
-      self.send :include, Transitions
-    end
-  end
-  
-  module InstanceMethods
-    def to_hash
-      self.serializable_hash.inject({}) do |result, (key, value)|
-        key, value = "id", value.to_s if key.to_s == "_id"
-        case value
-        when Date, DateTime, Time
-          value = value.to_s
-        end
-        result[key] = value
-        result
+      def has_transitions
+        self.send :include, Transitions
       end
     end
-    
-    def to_msgpack(*args)
-      self.to_hash.to_msgpack(*args)
-    end
-  end
   
-  def self.included(receiver)
-    receiver.extend         ClassMethods
-    receiver.send :include, InstanceMethods
-    receiver.send :include, Mongoid::Document
-    receiver.send :include, Mongoid::Timestamps
-    receiver.attr_protected :_id
+    module InstanceMethods
+      def to_hash
+        self.serializable_hash.inject({}) do |result, (key, value)|
+          key, value = "id", value.to_s if key.to_s == "_id"
+          case value
+          when Date, DateTime, Time
+            value = value.to_s
+          end
+          result[key] = value
+          result
+        end
+      end
+    
+      def to_msgpack(*args)
+        self.to_hash.to_msgpack(*args)
+      end
+    end
+  
+    def self.included(receiver)
+      receiver.extend         ClassMethods
+      receiver.send :include, InstanceMethods
+      receiver.send :include, Mongoid::Document
+      receiver.send :include, Mongoid::Timestamps
+      receiver.attr_protected :_id
+    end
   end
 end
