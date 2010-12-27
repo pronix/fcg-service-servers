87ad8e726382e3322ae04135f3afd8ebacc79796
Samuel O. Obukwelu <sam@fcgmedia.com>
2010-12-27 03:37:06 -0500

    minor changes to deploy #2

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,3 +1,4 @@
-# %W{ ampq app aws mongodb redis }.each do |name|
-#   run "ln -nfs #{shared_path}/config/settings/#{name}.yml #{release_path}/lib/fcg-service-servers/config/settings/#{name}.yml"
-# end
+raise "in here"
+%W{ ampq app aws mongodb redis }.each do |name|
+  run "ln -nfs #{shared_path}/config/settings/#{name}.yml #{release_path}/lib/fcg-service-servers/config/settings/#{name}.yml"
+end
