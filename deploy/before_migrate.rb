a405c27c171002c266374a188e07c320da90c1d7
Samuel O. Obukwelu <sam@fcgmedia.com>
2011-01-04 22:53:55 -0500

    minor change to before_migrate #3

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,7 +1,5 @@
-# raise "in here"
-  puts "shared_path:#{@shared_path}"
-  puts "current_release:#{@current_release}"
-%W{ amqp aws mongodb redis }.each do |name|
-  puts "name:#{name}"
-  # run "ln -nfs #{@shared_path}/config/settings/#{name}.yml #{@current_release}/lib/fcg-service-servers/config/settings/#{name}.yml"
-end
+# puts "shared_path:#{@shared_path}"
+# puts "current_release:#{@current_release}"
+# # %W{ amqp aws mongodb redis }.each do |name|
+# #   run "ln -nfs #{@shared_path}/config/settings/#{name}.yml #{@current_release}/lib/fcg-service-servers/config/settings/#{name}.yml"
+# # end
